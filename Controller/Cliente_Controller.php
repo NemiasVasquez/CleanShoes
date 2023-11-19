8b7af0621a82435bbc65cdb3cf593b37a011468d
NemiasVasquez <nemiasvasquezs@hotmail.com>
2023-11-19 10:26:18 -0500

    Model - Controller Registrar

--- Controller/Cliente_Controller.php
+++ Controller/Cliente_Controller.php
@@ -7,18 +7,68 @@ class Cliente_Controller{
         $this->Cliente_Modelo = new Cliente_Model();
     }
 
-    public function Registrar($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol){
-        $nombre = $_POST["nombre"];
-        $apellidos = $_POST["apellidos"];
-        $dni = $_POST["dni"];
-        $correo = $_POST["correo"];
-        $celular = $_POST["celular"];
-        $fechaNac= $_POST["fechaNac"];
-        $usuario  = $_POST["usuario"];
-        $contraseña = $_POST["contraseña"];
-        $rol = $_POST["rol"];
-        $this->Cliente_Modelo->setCliente($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol);
+    public function ValidarRegistro($nombre,$apellidos,$dni,$celular,$fechaNac,$usuario,$contraseña, $rol){
+        if($nombre==""){
+            return $mensaje="Debe ingresar un nombre"; 
+        }
+        if($apellidos==""){
+            return $mensaje ="Debe ingresar los apellidos";
+        }
+        if(strlen($dni)!=8){
+            return $mensaje= "Debe  ingresar un DNI válido";
+        }
+        if(strlen($celular)!=9){
+            return $mensaje ="Debe ingresar un número de celular válido";
+        }
+        if($fechaNac == ""){
+            return $mensaje ="Debe ingresar una fecha de nacimiento";
+        }
+        if($usuario ==""){
+            return $mensaje ="Debe ingresar un usuario";
+        }
+        if($contraseña==""){
+            return $mensaje ="Debe ingresar una contraseña.";
+        }else{
+            if(strlen($contraseña)<8){
+                return $mensaje ="Debe ser mayor a 8 caracteres";
+            }else{
+                if(!preg_match('/\d/',$contraseña)){
+                    return $mensaje="Debe tener almenos un número";
+                }
+            }
+        }
+        if($rol =="NA"){
+            return $mensaje="Debe seleccionar un roll.";
+        }
+        return $mensaje="";
+    }
 
+    public function Registrar($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol){
+        $mensaje=$this->ValidarRegistro($nombre,$apellidos,$dni,$celular,$fechaNac,$usuario,$contraseña, $rol);
+        if($mensaje!=""){
+            /* Debe estar un Require_One para llamar al formulario y pasar el mensaje de alerta */
+
+        }else{
+            $nombre = $_POST["nombre"];
+            $apellidos = $_POST["apellidos"];
+            $dni = $_POST["dni"];
+            $correo = $_POST["correo"];
+            $celular = $_POST["celular"];
+            $fechaNac= $_POST["fechaNac"];
+            $usuario  = $_POST["usuario"];
+            $contraseña = $_POST["contraseña"];
+            $rol = $_POST["rol"];
+
+            $consulta_Registro = $this->Cliente_Modelo->setCliente($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol);
+            
+            if($consulta_Registro){
+                $mensaje="Registro exitoso.";
+                /* Require_once Registrar */
+            }else{
+                $mensaje="No se ha logrado registrar el cliente.";
+                /* Require_once Registrar */
+            }
+        }
     }
 
 }
--- Model/Cliente_Model.php
+++ Model/Cliente_Model.php
@@ -8,18 +8,24 @@ class Cliente_Model{
         $this->Cliente=[];
     }
 
+    /* Falta validar para que retorne TRUE en caso sea todo correcto o FALSE en caso algo haya fallado -> Evitar Anidaciones */
     public function setCliente($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol){
-        $consulta_Persona = $this->dataBase->query("INSERT INTO persona(nombres,apellidos,dni,correo,celular,fecha_nac) VALUES ('$nombre','$apellidos','$dni','$correo','$celular','$fechaNac')");
+        $Registro_Persona = $this->dataBase->query("INSERT INTO persona(nombres,apellidos,dni,correo,celular,fecha_nac) VALUES ('$nombre','$apellidos','$dni','$correo','$celular','$fechaNac')");
+        
         $Consulta_id_Persona = $this->dataBase->query("SELECT id_Persona FROM persona WHERE dni = '$dni'");
         while ($fila_persona = $Consulta_id_Persona->fetch_assoc()){
             $codigo_Persona = $fila_persona['id_Persona'];    
         }
-        $consulta_Usuario = $this->dataBase->query("INSERT INTO usuario(usuario,'password',rol,estado) VALUES ('$usuario','$password','$rol','Activo')");
+
+        $Registro_Usuario = $this->dataBase->query("INSERT INTO usuario(usuario,password,rol,estado) VALUES ('$usuario','$contraseña','$rol','Activo')");
+        
         $Consulta_id_Usuario = $this->dataBase->query("SELECT id_Usuario FROM usuario AS U WHERE (U.usuario = '$usuario' AND U.password='$contraseña'");
         while ($fila_Usuario = $Consulta_id_Usuario->fetch_assoc()){
             $codigo_Usuario = $fila_Usuario['id_Usuario'];    
         }
-        $consulta_Cliente = $this->dataBase->query("INSERT INTO cliente(id_Persona,id_Usuario) VALUES ('$codigo_Persona','$codigo_Usuario')");
+
+        $Registro_Cliente = $this->dataBase->query("INSERT INTO cliente(id_Persona,id_Usuario) VALUES ('$codigo_Persona','$codigo_Usuario')");
+        
     }
 }
 ?>
