2583a544daecd74527ee1c746d72c3c02a8e5dc4
NemiasVasquez <nemiasvasquezs@hotmail.com>
2023-11-18 23:07:12 -0500

    Modificación Model Cliente

--- Model/Cliente_Model.php
+++ Model/Cliente_Model.php
@@ -2,9 +2,14 @@
 class Cliente_Model{
     private $dataBase;
     private $Cliente;
+
     public function __construct(){
         $this->dataBase = Conexion::Conexion();
         $this->Cliente=[];
+    }
+
+    public function Registrar($nombre,$apellidos,$dni,$correo,$celular,$fechaNac){
+        
     }
 }
 ?>
