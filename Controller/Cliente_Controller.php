c56af0e088f87a47dba7f3c9ac9cb0b8d8de6109
NemiasVasquez <nemiasvasquezs@hotmail.com>
2023-11-19 00:21:37 -0500

    Creación Set Model Controller Cliente

--- Controller/Cliente_Controller.php
+++ Controller/Cliente_Controller.php
@@ -7,6 +7,19 @@ class Cliente_Controller{
         $this->Cliente_Modelo = new Cliente_Model();
     }
 
-    
+    public function Registrar($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol){
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellidos"];
+        $dni = $_POST["dni"];
+        $correo = $_POST["correo"];
+        $celular = $_POST["celular"];
+        $fechaNac= $_POST["fechaNac"];
+        $usuario  = $_POST["usuario"];
+        $contraseña = $_POST["contraseña"];
+        $rol = $_POST["rol"];
+        $this->Cliente_Modelo->setCliente($nombre,$apellidos,$dni,$correo,$celular,$fechaNac,$usuario,$contraseña, $rol);
+
+    }
+
 }
 ?>
