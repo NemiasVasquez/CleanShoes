af668d3281521f257c300f6492cf433458cddd03
NemiasVasquez <nemiasvasquezs@hotmail.com>
2023-12-11 22:41:59 -0500

    Carrito - AÑADIR PRODUCTOS

--- Views/Carrito_Views.php
+++ Views/Carrito_Views.php
@@ -8,6 +8,7 @@
     <?php include 'Plantillas/Links.php'; ?>
 
     <link rel="stylesheet" href="Style/Carrito_Style.css">
+    <script type="text/javascript" src="JavaScript/Carrito_Ajax.js"></script>
     <title>Carrito de comnpras - CleanShoes</title>
 </head>
 <body>
@@ -17,68 +18,88 @@
     </header>
 
     <main>
-        <?php if(isset($data["ServicioVenta"]) && $data["ServicioVenta"] != false ){ ?>
-            <div id="bloque_TablaVenta">
-            <table class="table  text-center">
-                <thead>
-                    <tr>
-                        <th class="text-center" scope="col">#</th>
-                        <th class="text-center" scope="col">ID</th>
-                        <th class="text-center" scope="col">NOMBRE</th>
-                        <th class="text-center" scope="col">CATEGORÍA</th>
-                        <th class="text-center" scope="col">CANTIDAD</th>
-                        <th class="text-center" scope="col">PRECIO UNI.</th>
-                        <th class="text-center" scope="col">SUBTOTAL</th>
-                        <th class="text-center" scope="col">ACCIONES</th>
-                        <th class="text-center" scope="col"></th>
-                    </tr>
-                </thead>
-                <?php $contador=1 ?>
-           
-                <?php foreach ($data["ServicioVenta"] as $S): ?>
-                        <tr>
-                            <th class="text-center" scope="row"><?php echo $contador ?></th>
-                            <td><?php echo $S["id_Servicio"]; ?></td>
-                            <td><?php echo $S["nombre"]; ?></td>
-                            <td><?php echo $S["categoria"]; ?></td>
-                            <td><?php echo $S["cantidad"]; ?></td>
-                            <td><?php echo $S["precio"]; ?></td>
-                            <td><?php echo ($S["precio"]*$S["cantidad"]); ?></td>
-                            <td>botoncitos rojos</td>
-                        </tr>
-                        <?php $contador++; ?>
-                <?php endforeach; ?>
-            </table>
-            </div>
-            <div id="seccion_detalleVenta">
-                <div id="bloque_Indicaciones">
-                    <label>INDICACIONES ADICIONALES (OPCIONAL)</label>
-                    <textarea name="textarea_Indicaciones" rows="10" cols="50">Déjanos un mensaje</textarea>
+        <div>
+            <?php if(isset($data["ServicioVenta"]) && $data["ServicioVenta"] != false ){ ?>
+                <div id="bloque_TablaVenta">
+                    <div id="bloque_Titulo">
+                        <h2>Carrito de compras</h2>
+                    </div>
+                    <table class="table  text-center">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>ID</th>
+                                <th>NOMBRE</th>
+                                <th>CATEGORÍA</th>
+                                <th>CANTIDAD</th>
+                                <th>PRECIO UNI.</th>
+                                <th>SUBTOTAL</th>
+                                <th>ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <?php $contador=1 ?>
+                
+                        <?php foreach ($data["ServicioVenta"] as $S): ?>
+                                <tr class="tabla_elementos">
+                                    <th scope="row"><?php echo $contador ?></th>
+                                    <th><?php echo $S["id_Servicio"]; ?></th>
+                                    <td><?php echo $S["nombre"]; ?></td>
+                                    <td><?php echo $S["categoria"]; ?></td>
+                                    <td><?php echo $S["cantidad"]; ?></td>
+                                    <td><?php echo $S["precio"]; ?></td>
+                                    <td><?php echo ($S["precio"]*$S["cantidad"]); ?></td>
+                                    <td id="celda_Acciones">
+                                        <img src="Imagenes/Carrito/restar.png" alt="Imagen para restar servicios">
+                                        <p id="contadorServicios"></p>
+                                        <img src="Imagenes/Carrito/sumar.png" alt="Imagen para sumar servicios">
+                                        <img src="Imagenes/Carrito/eliminar.png" alt="Imagen para eliminar un producto">
+                                    </td>
+                                </tr>
+                                <?php $contador++; ?>
+                        <?php endforeach; ?>
+                    </table>
                 </div>
-                <div id="bloque_DetalleTotal">
+                <div id="seccion_detalleVenta">
+                    <div id="bloque_Indicaciones">
+                        <div>
+                            <label>INDICACIONES ADICIONALES (opcional)</label>
+                            <textarea name="textarea_Indicaciones" rows="10" cols="50">Déjanos un mensaje</textarea>
+                        </div>
+                    </div>
+                    <div id="bloque_DetalleTotal">
+                        <div>
+                            <div class="bloque_Precio">
+                                <p class="texto_Valor">SUB TOTAL:</p>
+                                <p class="bloque_Valor" id="subTotalPago"></p>
+                            </div>
+                            <div class="bloque_Precio">
+                                <p class="texto_Valor">IGV:</p>
+                                <p class="bloque_Valor" id="IGV"></p>
+                            </div>
+                            <div class="bloque_Precio">
+                                <p class="texto_Valor">TOTAL A PAGAR:</p>
+                                <p class="bloque_Valor" id="totalPago"></p>
+                            </div>
+                        </div>
+                       
+                        <div id="bloque_btn_Reservar">
+                            <button name="btn_Reservar" id="btn_Reservar">Reservar</button>
+                        </div>
+                    </div>
+                    
+                </div>
+            <?php } else { ?>
+                <div id="bloque_CarroVacio">
                     <div>
-                        <h2>SUB TOTAL:</h2>
-                        <h2 id="subTotalPago"></h2>
+                        <h3>No tiene productos en su carrito</h3>
                     </div>
                     <div>
-                        <h2>IGV:</h2>
-                        <h2 id="IGV"></h2>
-                    </div>
-                    <div>
-                        <h2>TOTAL A PAGAR:</h2>
-                        <h2 id="totalPago"></h2>
+                        <img src="Imagenes/Carrito/carro-vacio.png" alt="Carrito Vacío.">
                     </div>
                 </div>
-                <div>
-                    <button name="btn_Pagar" id="btn_Pagar">Ir a pagar</button>
-                </div>
-            </div>
-        <?php } else { ?>
-            <div>
-                <h3>No tiene productos en su carrito</h3>
-            </div>
-        <?php }?>
-
+               
+            <?php }?>
+        </div>
     </main>
     <aside>
         <?php include 'Plantillas/Enlaces.php'; ?>
