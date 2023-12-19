9b653255a4a18e4453ecba563b751f0061a8169d
NemiasVasquez <nemiasvasquezs@hotmail.com>
2023-12-19 17:13:19 -0500

    Actualizar Base

--- cleanshoes_Actualizada.sql
+++ cleanshoes_Actualizada.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-12-2023 a las 21:56:26
+-- Tiempo de generación: 19-12-2023 a las 23:07:23
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.1.12
 
